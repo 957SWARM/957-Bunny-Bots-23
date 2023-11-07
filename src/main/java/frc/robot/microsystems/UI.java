6487779fa84dd5b1fd0e5b8b82985cde53f5e80d
Jerry...or is it? <guoguo1813@gmail.com>
2023-11-06 18:33:24 -0800

    Started basic Shuffleboard code in accordance to the pieces of data that we set on the style guide.

--- src/main/java/frc/robot/microsystems/UI.java
+++ src/main/java/frc/robot/microsystems/UI.java
@@ -5,6 +5,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public final class UI {
 
     int ballCount = 0;
+    int shooterSpeed = 0;
+    int speedX = 0;
+    int speedY = 0;
+    int speedMagnitude = 0;
+    boolean bunnyGrabber;
     
     private static UI INSTANCE;
 
@@ -19,10 +24,35 @@ public final class UI {
 
 
     public void periodic(){
-        SmartDashboard.putNumber("#", 0);
+        SmartDashboard.putNumber("# of Balls", ballCount);
+        SmartDashboard.putNumber("Shooter Speed", shooterSpeed);
+        SmartDashboard.putNumber("Robot Speed X", speedX);
+        SmartDashboard.putNumber("Robot Speed Y", speedY);
+        SmartDashboard.putNumber("Robot Speed Magnitude", speedMagnitude);
+        SmartDashboard.putString("State of Bunny Grabber", grabberState(bunnyGrabber));
     }
 
     public void setBallCount(int ballCount){
         this.ballCount = ballCount;
+    }
+
+    public void setShooterSpeedNumber(int shooterSpeed){
+        this.shooterSpeed = shooterSpeed;
+    }
+
+    public void setRobotSpeed(int speedX, int speedY, int speedMagnitude){
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.speedMagnitude = speedMagnitude;
+    }
+    
+    public String grabberState(boolean bunnyGrabber){
+
+        if(bunnyGrabber = true){
+            return "Extended";
+        }
+        else{
+            return "Retracted";
+        }
     }
 }
