5975f1668ef5814d4e1d10721eb69dfa2b9fb38b
Thomas Anderson <tande92834@gmail.com>
2023-11-24 18:57:37 -0800

    pdh and ph are peripherals, actually

--- src/main/java/frc/robot/subsystems/BunnyGrabberSubsystem.java
+++ src/main/java/frc/robot/subsystems/BunnyGrabberSubsystem.java
@@ -2,23 +2,20 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.peripherals.PneumaticsHub;
 import frc.robot.peripherals.UI;
 
 public class BunnyGrabberSubsystem extends SubsystemBase {
 
-    private final DoubleSolenoid piston;
+    private final DoubleSolenoid piston =
+            PneumaticsHub.instance.getDoubleSolenoid(
+                    Constants.BunnyGrabberConstants.FORWARD_PORT,
+                    Constants.BunnyGrabberConstants.REVERSE_PORT);
 
     public BunnyGrabberSubsystem() {
-        piston =
-                new DoubleSolenoid(
-                        Constants.BunnyGrabberConstants.MODULE_PORT,
-                        PneumaticsModuleType.REVPH,
-                        Constants.BunnyGrabberConstants.FORWARD_PORT,
-                        Constants.BunnyGrabberConstants.REVERSE_PORT);
         piston.set(Value.kReverse);
     }
 
